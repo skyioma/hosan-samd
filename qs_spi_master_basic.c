e579ffb4372961f0398c018bc0f08b3e7e364be8
Foti To <skyioma42@gmail.com>
2021-03-28 23:57:01 +0200

    Apply project code style to qs_spi_master_basic.c

--- qs_spi_master_basic.c
+++ qs_spi_master_basic.c
@@ -30,16 +30,15 @@
  * \asf_license_stop
  *
  */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
+
 #include <asf.h>
 
 #define BUF_LENGTH 20
 #define SLAVE_SELECT_PIN CONF_MASTER_SS_PIN
+
 static uint8_t buffer[BUF_LENGTH] = {
-		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
-		0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+    0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13
 };
 
 struct spi_module spi_master_instance;
@@ -49,38 +48,39 @@ void configure_spi_master(void);
 
 void configure_spi_master(void)
 {
-	struct spi_config config_spi_master;
-	struct spi_slave_inst_config slave_dev_config;
-	/* Configure and initialize software device instance of peripheral slave */
-	spi_slave_inst_get_config_defaults(&slave_dev_config);
-	slave_dev_config.ss_pin = SLAVE_SELECT_PIN;
-	spi_attach_slave(&slave, &slave_dev_config);
-	/* Configure, initialize and enable SERCOM SPI module */
-	spi_get_config_defaults(&config_spi_master);
-	config_spi_master.mux_setting = CONF_MASTER_MUX_SETTING;
-	config_spi_master.pinmux_pad0 = CONF_MASTER_PINMUX_PAD0;
-	config_spi_master.pinmux_pad1 = CONF_MASTER_PINMUX_PAD1;
-	config_spi_master.pinmux_pad2 = CONF_MASTER_PINMUX_PAD2;
-	config_spi_master.pinmux_pad3 = CONF_MASTER_PINMUX_PAD3;
+  struct spi_config config_spi_master;
+  struct spi_slave_inst_config slave_dev_config;
 
-	spi_init(&spi_master_instance, CONF_MASTER_SPI_MODULE, &config_spi_master);
+  /* Configure and initialize software device instance of peripheral slave */
+  spi_slave_inst_get_config_defaults(&slave_dev_config);
+  slave_dev_config.ss_pin = SLAVE_SELECT_PIN;
+  spi_attach_slave(&slave, &slave_dev_config);
 
-	spi_enable(&spi_master_instance);
+  /* Configure, initialize and enable SERCOM SPI module */
+  spi_get_config_defaults(&config_spi_master);
+  config_spi_master.mux_setting = CONF_MASTER_MUX_SETTING;
+  config_spi_master.pinmux_pad0 = CONF_MASTER_PINMUX_PAD0;
+  config_spi_master.pinmux_pad1 = CONF_MASTER_PINMUX_PAD1;
+  config_spi_master.pinmux_pad2 = CONF_MASTER_PINMUX_PAD2;
+  config_spi_master.pinmux_pad3 = CONF_MASTER_PINMUX_PAD3;
 
+  spi_init(&spi_master_instance, CONF_MASTER_SPI_MODULE, &config_spi_master);
+
+  spi_enable(&spi_master_instance);
 }
 
 int main(void)
 {
-	system_init();
-	configure_spi_master();
+  system_init();
+  configure_spi_master();
 
-	while (true) {
-		/* Infinite loop */
-		if(!port_pin_get_input_level(BUTTON_0_PIN)) {
-			spi_select_slave(&spi_master_instance, &slave, true);
-			spi_write_buffer_wait(&spi_master_instance, buffer, BUF_LENGTH);
-			spi_select_slave(&spi_master_instance, &slave, false);
-			port_pin_set_output_level(LED_0_PIN, LED0_ACTIVE);
-		}
-	}
+  while (true) {
+    /* Infinite loop */
+    if (!port_pin_get_input_level(BUTTON_0_PIN)) {
+      spi_select_slave(&spi_master_instance, &slave, true);
+      spi_write_buffer_wait(&spi_master_instance, buffer, BUF_LENGTH);
+      spi_select_slave(&spi_master_instance, &slave, false);
+      port_pin_set_output_level(LED_0_PIN, LED0_ACTIVE);
+    }
+  }
 }
