749b69c98eda8fd02128a7d1c59ebc30a155387e
Foti To <skyioma42@gmail.com>
2021-03-30 21:35:01 +0200

    Fix FreeRTOS MCU frequency

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -50,7 +50,7 @@ void assert_triggered( const char * file, uint32_t line );
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
 #define configPRIO_BITS                         2
-#define configCPU_CLOCK_HZ                      ( 48000000 )
+#define configCPU_CLOCK_HZ                      ( 8000000 )
 #define configTICK_RATE_HZ                      ( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES                    ( 5 )
 #define configMINIMAL_STACK_SIZE                ( ( unsigned short ) 100 )
