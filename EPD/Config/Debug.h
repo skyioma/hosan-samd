31e87152f7da44f3bbbacbb576bb4504d3f363f3
Foti To <skyioma42@gmail.com>
2021-04-03 20:37:33 +0200

    Disable EPD debug (for now)

--- EPD/Config/Debug.h
+++ EPD/Config/Debug.h
@@ -14,11 +14,14 @@
 #ifndef __DEBUG_H
 #define __DEBUG_H
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 	#define Debug(__info,...) printf("Debug: " __info,##__VA_ARGS__)
 #else
 	#define Debug(__info,...)  
 #endif
+
+// TODO Improve it!
+#define iprintf(...)
 
 #endif
