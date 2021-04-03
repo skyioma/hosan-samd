6a543a6332ada8fdbc2f89f6daf132eb416c47fe
Foti To <skyioma42@gmail.com>
2021-04-03 20:07:00 +0200

    Apply project code style to main.c

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 /**
  * \file
  *
- * \brief Debug print basic quick start guide code
- *
  * Copyright (c) 2014-2018 Microchip Technology Inc. and its subsidiaries.
  *
  * \asf_license_start
@@ -30,61 +28,40 @@
  * \asf_license_stop
  *
  */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
 
 #include <asf.h>
 
 #include "hal_spi.h"
 
-//! [main_task_data]
 uint32_t main_counter;
 char main_string[] = "Main task iteration: 0x00000000\r\n";
-//! [main_task_data]
 
-//! [main_task]
-//! [main_task_open]
 static void main_task(void *params)
 {
   do {
-//! [main_task_open]
-//! [main_task_1]
     dbg_print_str("Main task loop executing\r\n");
-//! [main_task_1]
 
-//! [main_task_2]
     // Update hexadecimal 32-bit integer in string, and print it
     dbg_sprint_hexint(&main_string[23], main_counter++);
     dbg_print_str(main_string);
-//! [main_task_2]
 
-//! [main_task_close]
     vTaskDelay(1000 / portTICK_RATE_MS);
   } while(1);
 }
-//! [main_task_close]
-//! [main_task]
 
 
-int main (void)
+int main(void)
 {
-//! [init_calls]
   system_init();
   dbg_init();
   hal_spi_init();
-//! [init_calls]
 
-//! [main_task_create]
   xTaskCreate(&main_task,
     (const char *)"Main task",
     configMINIMAL_STACK_SIZE + 100,
     NULL,
     tskIDLE_PRIORITY + 2,
     NULL);
-//! [main_task_create]
 
-//! [freertos_start]
   vTaskStartScheduler();
-//! [freertos_start]
 }
