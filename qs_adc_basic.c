73c48cc5e9fb904d7090c9625629e7de853ad6d5
Foti To <skyioma42@gmail.com>
2021-04-10 16:25:54 +0200

    Apply project code style to qs_adc_basic.c

--- qs_adc_basic.c
+++ qs_adc_basic.c
@@ -37,59 +37,37 @@
 
 void configure_adc(void);
 
-//! [module_inst]
 struct adc_module adc_instance;
-//! [module_inst]
 
-//! [setup]
 void configure_adc(void)
 {
-//! [setup_config]
 	struct adc_config config_adc;
-//! [setup_config]
-//! [setup_config_defaults]
 	adc_get_config_defaults(&config_adc);
-//! [setup_config_defaults]
 
-//! [setup_set_config]
 #if (SAMC21)
 	adc_init(&adc_instance, ADC1, &config_adc);
 #else
 	adc_init(&adc_instance, ADC, &config_adc);
 #endif
-//! [setup_set_config]
 
-//! [setup_enable]
 	adc_enable(&adc_instance);
-//! [setup_enable]
 }
-//! [setup]
 
 int main(void)
 {
 	system_init();
 
-//! [setup_init]
 	configure_adc();
-//! [setup_init]
 
-//! [main]
-//! [start_conv]
 	adc_start_conversion(&adc_instance);
-//! [start_conv]
 
-//! [get_res]
 	uint16_t result;
 
 	do {
 		/* Wait for conversion to be done and read out result */
 	} while (adc_read(&adc_instance, &result) == STATUS_BUSY);
-//! [get_res]
 
-//! [inf_loop]
 	while (1) {
 		/* Infinite loop */
 	}
-//! [inf_loop]
-//! [main]
 }
